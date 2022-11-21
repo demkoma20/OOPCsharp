45a813110347e7161c93fa95b9c7e26ae92acd8a
demkoma20 <demkoma20@sps-prosek.cz>
2022-11-21 16:05:13 +0100

    adv

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace OOP
+{
+	class Program
+	{
+		static void Main()
+		{
+			// ...
+			
+			Zak zak1 = new Zak();
+			zak1.Jmeno = "Petr";
+			zak1.Prijmeni = "Pan";
+			zak1.RokNarozeni = 1021;
 
-Console.WriteLine("Hello, World!");
+			Zak zak2 = new Zak()
+			{
+				Jmeno = "Kapitan",
+				Prijmeni = "Hook",
+				RokNarozeni = 1002
+			};
+
+			Zak zak3 = new Zak("Jirka", "Král", 1996);
+			Zak zak4 = new Zak("Víla", "Amalka");
+			
+			zak1.Pozdrav();
+		}
+	}
+}
---
+++ OOP/Zak.cs
@@ -0,0 +1,35 @@
+﻿namespace OOP
+{
+	internal class Zak
+	{
+		internal string Jmeno;
+		internal string Prijmeni;
+		internal int RokNarozeni;
+		
+		internal Zak() { }
+
+		internal Zak(string _jmeno, string _prijmeni, int _rokNarozeni)
+		{
+			Jmeno = _jmeno;
+			Prijmeni = _prijmeni;
+			RokNarozeni = _rokNarozeni;
+		}
+		
+		internal Zak(string _jmeno, string _prijmeni)
+		{
+			Jmeno = _jmeno;
+			Prijmeni = _prijmeni;
+			RokNarozeni = -1;
+		}
+
+		private int VratVek()
+		{
+			return 2022 - RokNarozeni;
+		}
+
+		internal void Pozdrav()
+		{
+			Console.WriteLine($"Ahoj, já jsem: {Jmeno} a je mi {VratVek().ToString()} let.");
+		}
+	}
+}
